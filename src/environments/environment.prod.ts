cd658cfea6eb2f50ce645d2c2a335a4101fe92a7
Edison Augusthy <edisonaugusthy117@gmail.com>
2020-08-28 10:14:42 +0530

    env update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://businessapp.axistc.com/axis/api/',
+  // baseUrl: 'http://businessapp.axistc.com/axis/api/',
+  baseUrl: 'http://iroidtechnologies.in/axis_laravel/api/',
   imageBase: 'http://axiz.rancarts.com',
 };
